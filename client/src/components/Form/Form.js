790ed5b4770d6a2ee465c54c854ba006335bf78b
Alejandro <eloso85@gmail.com>
2020-11-11 20:08:56 -0600

    completed connection to mongo db

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,18 +4,26 @@ import {TextField, Button, Typography, Paper} from '@material-ui/core';
 
 import FileBase from 'react-file-base64';
 
+import {useDispatch} from 'react-redux';
+
+import {createPost} from '../../actions/posts'
+
 
 //styles
 import useStyles from './styles'
+//import { createPost } from '../../api';
 
 const Form = ()=>{
     const [postData, setPostData] = useState({
         creator:'', title:'', message:'',tags:'',selctedFile:'',
     })
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = ()=>{
 
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        dispatch(createPost(postData));
     }
 
     const clear =()=>{
@@ -24,7 +32,7 @@ const Form = ()=>{
 
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}` } onSubmit={handleSubmit}>
                 <Typography variant ="h6">Creating a Memory</Typography>
                 <TextField name="creator" variant="outlined" label="creator" fullWidth value={postData.creator} onChange={(e)=>setPostData({...postData, creator: e.target.value})}/>
                 <TextField name="title" variant="outlined" label="title" fullWidth value={postData.title} onChange={(e)=>setPostData({...postData, title: e.target.value})}/>
@@ -35,7 +43,7 @@ const Form = ()=>{
                     <FileBase 
                     type="file"
                     multiple={false}
-                    onDone={({base64})=>setPostData({...postData,selctedFile: base64})}
+                    onDone={({base64})=>setPostData({...postData,selectedFile: base64})}
                     />
                 </div>
 
